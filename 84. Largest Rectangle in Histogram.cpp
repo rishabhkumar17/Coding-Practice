174dc9abbcb6da728e183e06aa1e20dcc0e83010
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-08-29 05:49:48 +0530

    #stacks

--- 84. Largest Rectangle in Histogram.cpp
+++ 84. Largest Rectangle in Histogram.cpp
@@ -1,26 +1,70 @@
 class Solution
 {
+private:
+    vector<int> nextSmallerElement(vector<int> arr, int n)
+    {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            int num = arr[i];
+            while (s.top() != -1 && arr[s.top()] >= num)
+            {
+                s.pop();
+            }
+
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+
+    vector<int> prevSmallerElement(vector<int> arr, int n)
+    {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            int num = arr[i];
+            while (s.top() != -1 && arr[s.top()] >= num)
+            {
+                s.pop();
+            }
+
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+
 public:
     int largestRectangleArea(vector<int> &heights)
     {
+        int n = heights.size();
 
-        int ans = 0;
-        heights.push_back(0);
-        vector<int> index;
+        vector<int> next(n);
+        next = nextSmallerElement(heights, n);
 
-        for (int i = 0; i < heights.size(); i++)
+        vector<int> prev(n);
+        prev = prevSmallerElement(heights, n);
+
+        int area = INT_MIN;
+        for (int i = 0; i < n; i++)
         {
-            while (index.size() > 0 && heights[index.back()] >= heights[i])
-            {
-                int height = heights[index.back()];
-                index.pop_back();
+            int l = heights[i];
 
-                int sidx = index.size() > 0 ? index.back() : -1;
-                if (height * (i - sidx - 1) > ans)
-                    ans = height * (i - sidx - 1);
+            if (next[i] == -1)
+            {
+                next[i] = n;
             }
-            index.push_back(i);
+            int b = next[i] - prev[i] - 1;
+            int newArea = l * b;
+            area = max(area, newArea);
         }
-        return ans;
+        return area;
     }
 };
