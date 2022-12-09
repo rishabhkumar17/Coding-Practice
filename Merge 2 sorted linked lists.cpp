4e0f7157e70d65c8b2ebab61b86ee12cc8687c68
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-12-09 19:59:25 +0530

    Linked List

--- Merge 2 sorted linked lists.cpp
+++ Merge 2 sorted linked lists.cpp
@@ -16,3 +16,27 @@ ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         tail->next = l1 ? l1 : l2;
         return dummy.next;
 }
+
+/////////////////////
+
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+         ListNode* result = NULL; 
+     
+    if (l1 == NULL) 
+        return(l2); 
+    else if (l2 == NULL) 
+        return(l1); 
+     
+ 
+    if (l1->val <= l2->val) 
+    { 
+        result = l1; 
+        result->next = mergeTwoLists(l1->next, l2); 
+    } 
+    else
+    { 
+        result = l2; 
+        result->next = mergeTwoLists(l1, l2->next); 
+    } 
+    return(result); 
+    }
