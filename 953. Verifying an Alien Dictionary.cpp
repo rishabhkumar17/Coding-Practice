177ccbe027f9fa834b676adb0ae8ec7a94d3af19
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-12-27 20:22:50 +0530

    mapping

---
+++ 953. Verifying an Alien Dictionary.cpp
@@ -0,0 +1,13 @@
+bool isAlienSorted(vector<string> &words, string order)
+{
+    int mapping[26];
+    for (int i = 0; i < 26; i++)
+        mapping[order[i] - 'a'] = i;
+    for (string &w : words)
+        for (char &c : w)
+        {
+            c = mapping[c - 'a'] + 'a';
+            cout << w << endl;
+        }
+    return is_sorted(words.begin(), words.end());
+}
