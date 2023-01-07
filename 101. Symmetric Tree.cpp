225554dd2a39e8d0a59efa5f231b08d2832e9817
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-07 21:58:46 +0530

    BST

---
+++ 101. Symmetric Tree.cpp
@@ -0,0 +1,31 @@
+bool isSymmetric(TreeNode *root)
+{
+    // if(root==NULL) return true;
+    // return isMirror(root -> left, root-> right);
+    TreeNode *left, *right;
+    if (!root)
+        return true;
+
+    queue<TreeNode *> q1, q2;
+    q1.push(root->left);
+    q2.push(root->right);
+    while (!q1.empty() && !q2.empty())
+    {
+        left = q1.front();
+        q1.pop();
+        right = q2.front();
+        q2.pop();
+
+        if (left == NULL && right == NULL)
+            continue;
+        if (left == NULL || right == NULL)
+            return false;
+        if (left->val != right->val)
+            return false;
+        q1.push(left->left);
+        q1.push(left->right);
+        q2.push(right->right);
+        q2.push(right->left);
+    }
+    return true;
+}
