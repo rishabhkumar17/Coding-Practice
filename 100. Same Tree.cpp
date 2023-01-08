716125b4a5ebcc6029cb8cec4fc48a23d6d2c1e7
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-08 21:31:33 +0530

    BT

---
+++ 100. Same Tree.cpp
@@ -0,0 +1,11 @@
+bool isSameTree(TreeNode *p, TreeNode *q)
+{
+    if (!p && !q)
+        return true;
+    if ((!p && q) || (p && !q))
+        return false;
+
+    if (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right))
+        return true;
+    return false;
+}
