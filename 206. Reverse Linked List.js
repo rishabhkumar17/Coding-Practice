7e0e598c74df89c4b673cad02c58cb7f4a104719
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-01-19 05:15:59 +0530

    LinkedList iterative

---
+++ 206. Reverse Linked List.js
@@ -0,0 +1,14 @@
+//iterative solution
+var reverseList = function (head) {
+  let prev = null;
+  let curr = head;
+  let next = null;
+
+  while (curr !== null) {
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+};
